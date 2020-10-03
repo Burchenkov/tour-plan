a9c7d40000e6f354f24899816d64eedcca7f7a58
Burchenkov <db19012002@mail.ru>
2020-10-03 19:20:42 +0300

    Fix: adjusted the swiper for myself

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var mySwiper = new Swiper(".swiper-container", {
 
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
   },
 });

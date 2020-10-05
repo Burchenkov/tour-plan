b9f80bb579fc382031f9e14a8f19edfe4b1b13e6
Burchenkov <db19012002@mail.ru>
2020-10-06 01:35:23 +0300

    Create: IT fear

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-var mySwiper = new Swiper(".swiper-container", {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button-next",
-    prevEl: ".slider-button-prev",
+    nextEl: ".hotel-slider-button__next",
+    prevEl: ".hotel-slider-button__prev",
+  },
+});
+
+var reviwesSlider = new Swiper(".reviwes-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviwes-swiper-button__next",
+    prevEl: ".reviwes-swiper-button__prev",
   },
 });
